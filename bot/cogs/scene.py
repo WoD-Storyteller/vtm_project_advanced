414689c6716ecd96d129a304b2315c742b95b043
WoD-Storyteller <gurutim111@gmail.com>
2025-11-26 13:36:02 +0000

    update bot/cogs/scene.py

--- bot/cogs/scene.py
+++ bot/cogs/scene.py
@@ -1,14 +1,30 @@
+import discord
 from discord.ext import commands
-from core.vampires.scenes import generate_scene_description
+from core.vampires.scenes import generate_scene_response
 
-class Scene(commands.Cog):
+class SceneCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.active_scenes = {}
 
-    @commands.command(name="describe")
-    async def describe(self, ctx, *, location: str):
-        text = generate_scene_description(location)
-        await ctx.send(text)
+    @commands.group(name="scene", invoke_without_command=True)
+    async def scene(self, ctx):
+        await ctx.reply("Use: !scene start <location>, !scene end, !scene narrate <text>")
+
+    @scene.command(name="start")
+    async def scene_start(self, ctx, *, location):
+        self.active_scenes[ctx.channel.id] = location
+        await ctx.send(f"**Scene started in {location}.**")
+
+    @scene.command(name="end")
+    async def scene_end(self, ctx):
+        self.active_scenes.pop(ctx.channel.id, None)
+        await ctx.send("**Scene ended.**")
+
+    @scene.command(name="narrate")
+    async def scene_narrate(self, ctx, *, text):
+        resp = generate_scene_response(text)
+        await ctx.send(resp)
 
 async def setup(bot):
-    await bot.add_cog(Scene(bot))
+    await bot.add_cog(SceneCog(bot))
