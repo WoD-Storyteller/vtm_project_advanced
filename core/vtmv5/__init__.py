ae674b13ed55fdc96d494a8628247188d574f1f9
WoD-Storyteller <gurutim111@gmail.com>
2025-11-27 01:21:36 +0000

    update core/vtmv5/__init__.py

---
+++ core/vtmv5/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from . import dice
+from . import hunger
+from . import willpower
+from . import humanity
+from . import frenzy
+from . import blood_potency
+from . import predator_types
+from . import tenets
+from . import character_model
+
+__all__ = [
+    "dice",
+    "hunger",
+    "willpower",
+    "humanity",
+    "frenzy",
+    "blood_potency",
+    "predator_types",
+    "tenets",
+    "character_model",
+]
